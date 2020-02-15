a124b939ab5f32125df39a6355fad36bf881e5f8
Zhi Li <zhi.li@invoicefair.com>
2020-02-15 21:24:43 +0000

    added circuit breaker

--- cloud-security/orderservice/src/main/java/com/orderservice/order/OrderController.java
+++ cloud-security/orderservice/src/main/java/com/orderservice/order/OrderController.java
@@ -1,6 +1,7 @@
 package com.orderservice.order;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 
 @Slf4j
 @RestController
@@ -29,14 +32,25 @@ public class OrderController {
 //  @PreAuthorize("#hasRole('ROLE_ADMIN')")
   @PreAuthorize("#oauth2.hasScope('write')")// has to have write in scope
   @GetMapping("/{id}")
-  @SentinelResource("getOrder")
-  public OrderInfo getById(@PathVariable final Long id, @AuthenticationPrincipal String username) {
+  @SentinelResource(value = "getOrder", blockHandler = "getOrderFallback")
+  public OrderInfo getById(@PathVariable final Long id, @AuthenticationPrincipal String username) throws InterruptedException {
     log.info("Logged in user: {}", username);
     OrderInfo orderInfo = new OrderInfo();
     PriceInfo priceInfo = restTemplate.getForObject("http://localhost:8081/prices/" + 200, PriceInfo.class);
     orderInfo.setOrderId(id);
     orderInfo.setProductId(priceInfo.getId());
     orderInfo.setPrice(priceInfo.getPrice());
+    Thread.sleep(50);
+
+    return orderInfo;
+  }
+
+  public OrderInfo getOrderFallback(@PathVariable final Long id, @AuthenticationPrincipal String username, BlockException blockException) {
+    log.info("Fallback executing...");
+    OrderInfo orderInfo = new OrderInfo();
+    orderInfo.setOrderId(id);
+    orderInfo.setProductId(20L);
+    orderInfo.setPrice(BigDecimal.ONE);
 
     return orderInfo;
   }
--- cloud-security/orderservice/src/main/java/com/orderservice/sentinel/SentinelConfig.java
+++ cloud-security/orderservice/src/main/java/com/orderservice/sentinel/SentinelConfig.java
@@ -1,6 +1,8 @@
 package com.orderservice.sentinel;
 
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.context.ApplicationListener;
@@ -19,10 +21,19 @@ public class SentinelConfig implements ApplicationListener<ContextRefreshedEvent
     FlowRule rule = new FlowRule();
     rule.setResource("getOrder");
     rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
-    rule.setCount(1);
+    rule.setCount(10); // 10 requests in 1s
     List<FlowRule> rules = new ArrayList<>();
     rules.add(rule);
     FlowRuleManager.loadRules(rules);
+
+    DegradeRule degradeRule = new DegradeRule();
+    degradeRule.setResource("getOrder");
+    degradeRule.setGrade(RuleConstant.DEGRADE_GRADE_RT);
+    degradeRule.setCount(10); // if response time greater than 10ms
+    degradeRule.setTimeWindow(10); // circuit break time (10s)
+    List<DegradeRule> degradeRules = new ArrayList<>();
+    degradeRules.add(degradeRule);
+    DegradeRuleManager.loadRules(degradeRules);
   }
 
 }
